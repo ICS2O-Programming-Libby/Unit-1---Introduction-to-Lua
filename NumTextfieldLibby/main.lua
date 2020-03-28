3962bf71d9b2a23e66ce1ddd168b52c2c6eb4bb7
Libby Valentino <61517016+s276237@users.noreply.github.com>
2020-03-28 19:12:29 -0400

    finished

--- NumTextfieldLibby/main.lua
+++ NumTextfieldLibby/main.lua
@@ -24,6 +24,11 @@ local randomNumber1
 local randomNumber2
 local userAnswer
 local correctAnswer
+local incorrectAnswer
+local points = 0
+local incorrectPoints = 0
+local displayCorrectAnswer
+
 
 -----------------------------------------------------------------------------------------
 --LOCAL FUNCTIONS
@@ -36,24 +41,48 @@ local function AskQuestion()
 
 	--set the correct answer
 	correctAnswer = randomNumber1 + randomNumber2
-
-	--create a quesrion in the text object
+	print("Correct answer is : " .. randomNumber1 + randomNumber2)
+	--displayCorrectAnswer = totext(randomNumber1 + randomNumber2)
+	--correctAnswer.text = tostring(correctAnswer)
+	--local displayCorrectAnswer = correctAnswer
+	--create a question in the text object
 	questionObject.text = randomNumber1.."+".. randomNumber2.."="
 
 end
 
 local function HideCorrect()
-	-- hide the correct answer
+	-- hide the correct answer text
 	correctObject.isVisible = false
 	AskQuestion()
 end
 
-local function NumericFieldListener( event )
+
+local function HideIncorrect()
+	--hide the incorrect answer text
+	incorrectObject.isVisible = false 
+	AskQuestion()
+end
+
+local function HideWin()
+	-- hide the win image 
+	win.isVisible = false  
+	AskQuestion()
+end
+
+local function HideGameOver()
+	-- hide the game over image 
+	win.isVisible = false  
+	AskQuestion()
+end
+
+
+local function NumericFieldListener ( event )
 	-- user begins editing "numericfield"
 	if ( event.phase == "began") then 
 		--clear text field 
 		
 		event.target.text = ""
+		
 
 	elseif event.phase == "submitted" then 
 
@@ -64,11 +93,32 @@ local function NumericFieldListener( event )
 		if (userAnswer == correctAnswer) then
 			correctObject.isVisible = true
 			timer.performWithDelay(2000, HideCorrect)
+			--give the user a point 
+			points = points + 1 
+			pointsText.text = "points = " .. points
+			if (points == 5) then 
+				win.isVisible = true 
+			end
+
+		else 
+			print (correctAnswer)
+			--displayCorrectAnswer.text = tostring(correctAnswer)
+			incorrectObject.isVisible = true 
+			timer.performWithDelay(2000, HideIncorrect)
+			--displayCorrectAnswer = tostring(correctAnswer)
+			print(displayCorrectAnswer)
+			-- add a point to the incorrect points tally
+            incorrectPoints = incorrectPoints + 1
+            if (incorrectPoints == 3) then
+            	gameOver.isVisible = true 
+            end
+
 		end
-		--clear the text field
+        --clear the text field
 		event.target.text = ""
 	end 
 end 
+
 
 
 
@@ -77,20 +127,41 @@ end
 ---------------------------------------------------------------------------------------------
 
 --displays a question and sets the colour 
-questionObject = display.newText("", display.contentWidth/3, display.contentHeight/2, nil, 50)
+questionObject = display.newText("",display.contentWidth/3, display.contentHeight/2, nil, 55)
 questionObject:setTextColor(195/255, 147/255, 200/255)
 
 --create the correct text object and make it invisible 
-correctObject = display.newText("",  display.contentWidth/3, display.contentHeight*2/3, nil, 50)
+correctObject = display.newText("Correct!", 512, 500, nil, 50)
 correctObject:setTextColor(195/255, 147/255, 200/255)
 correctObject.isVisible = false
 
+--create the Incorrect text object and ake it invisible
+incorrectObject = display.newText("Incorrect!", 512, 500, nil, 30)
+--incorrectObject = display.newText( tostring(displayCorrectAnswer), 512, 500, nil, 30)
+incorrectObject:setTextColor(109/255, 86/255, 137/255)
+incorrectObject.isVisible = false
+
 --create numeric field
-numericField = native.newTextField( display.contentWidth/2, display.contentHeight/2, 150, 80 )
+numericField = native.newTextField( 512, 385, 150, 70 )
 numericField.inputType = "number"
 
 --add the event listner for the numeric field 
-numericField:addEventListener( "userInput", NumericFieldListener )
+numericField:addEventListener("userInput", NumericFieldListener)
+
+--create the points text
+pointsText = display.newText("Points = " .. points, 800, 100, nil, 50 ) 
+
+-- display Correct image and make it invisible
+win = display.newImageRect("Images/win.png", 300, 300)
+win.x = display.contentWidth/2
+win.y = display.contentHeight*1/8
+win.isVisible = false 
+
+--display incorrect image and make it invisible
+gameOver = display.newImageRect("Images/gameover.png", 300, 300)
+gameOver.x = display.contentWidth/2
+gameOver.y = display.contentHeight*6/8
+gameOver.isVisible = false 
 
 --------------------------------------------------------------------
 --FUNCTION CALLS
